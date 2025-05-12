f76102340096763561bb503f3c2d283d20bff7d4
MATH-286-Dell-Ubuntu <jhu114514@gmail.com>
2025-05-12 12:35:13 +0000

    完成距离墙 5cm 停止

--- src/thymio_mini_project/thymio_mini_project/command_node.py
+++ src/thymio_mini_project/thymio_mini_project/command_node.py
@@ -2,19 +2,40 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+m  = 1
+cm = 0.01
+mm = 0.001
 
 class MinimalPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('VelocityProcessor')
+
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/thymio_front_center_proximity_sensor',
+            self.sensor_callback,
+            10)
+
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+
+    def sensor_callback(self, msg: LaserScan):
+        self.dist_front = msg.ranges
+        # self.get_logger().info('Distance: {msg.ranges}')
+
+
     def timer_callback(self):
         msg = Twist()
-        msg.linear.x = 0.1
+        if self.dist_front[0] < 5*cm:
+            msg.linear.x = 0.0
+        else:
+            msg.linear.x = 0.1
         msg.linear.y = 0.0
         msg.linear.z = 0.0
         msg.angular.x = 0.0
@@ -22,8 +43,10 @@ class MinimalPublisher(Node):
         msg.angular.z = 0.0
 
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: {msg.linear.x}')
+        # self.get_logger().info('Publishing: {msg.linear.x}')
         self.i += 1
+    
+
 
 def main(args=None):
     rclpy.init(args=args)
